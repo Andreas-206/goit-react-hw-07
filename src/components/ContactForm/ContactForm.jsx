8e98a704e2fcf2a6a7e6d65bd4e34d012d1fcc66
Andreas-206 <puma_real@ukr.net>
2024-03-24 14:47:39 +0200

    -

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,25 @@
 import { Field, Form, Formik } from 'formik'
 import { useDispatch } from 'react-redux'
 import { addContact } from '../../redux/contactsOps.js'
-import { validationSchema } from '../../redux/validation'
 import css from './ContactForm.module.css'
 import { nanoid } from 'nanoid'
 import clsx from 'clsx'
+import * as Yup from 'yup'
+
+const phoneRegExp =
+	/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+export const validationSchema = Yup.object().shape({
+	name: Yup.string()
+		.min(3, 'Invalid number!')
+		.max(50, 'Too Long!')
+		.required('Required'),
+	number: Yup.string()
+		.matches(phoneRegExp, 'Phone number is not valid')
+		.min(10, 'Invalid number!')
+		.max(12, 'Too Long!')
+		.required('Required'),
+})
 
 const ContactForm = () => {
 	const dispatch = useDispatch()
